deb8cd6d7681d5c8177f6e8b158418151e0d090f
Juliano Faccioni <julianofaccioni@gmail.com>
2020-03-20 09:12:18 -0300

    Finished retouching the GUI

--- src/interface.py
+++ src/interface.py
@@ -31,10 +31,6 @@ class DynaFitGUI(QMainWindow):
         # --Main layout--
         main_layout = QVBoxLayout()
         self.frame.setLayout(main_layout)
-        # --Top bar--
-        # App header
-        top_bar = QVBoxLayout()
-        main_layout.addLayout(top_bar)
         # --Columns--
         # App body
         columns = QHBoxLayout()
@@ -48,15 +44,15 @@ class DynaFitGUI(QMainWindow):
         right_column = QVBoxLayout()
         columns.addLayout(right_column)
 
-        # ### TOP BAR (TITLE)
-
-        self.title = QLabel(self, text='DynaFit GUI', styleSheet='font-size: 16pt; font-weight: 600')
-        top_bar.addWidget(self.title)
-
         # ### LEFT COLUMN
+
+        # --DynaFit GUI title--
+        self.title = QLabel(self, text='DynaFit GUI', styleSheet='font-size: 16pt; font-weight: 600')
+        left_column.addWidget(self.title)
 
         # --Input frame--
         # Region where users select worksheet and spreadsheet to analyse
+        left_column.addWidget(QLabel(self, text='Input selection', styleSheet='font-weight: 600'))
         self.input_frame = QFrame(self, frameShape=QFrame.Box)
         self.input_frame.setLayout(QGridLayout())
         # Input button
@@ -74,88 +70,107 @@ class DynaFitGUI(QMainWindow):
         self.input_sheetname.addItem('No data yet')
         self.input_frame.layout().addWidget(self.input_sheetname_label, 2, 0)
         self.input_frame.layout().addWidget(self.input_sheetname, 2, 1)
-        # add section above to left column
+        # Add section above to left column
         self.input_frame.layout()
         left_column.addWidget(self.input_frame)
 
         # --Cell range frame--
         # Region where user selects the ranges of cells containing CS and GR data
+        left_column.addWidget(QLabel(self, text='Cell range selection', styleSheet='font-weight: 600'))
         self.cell_range_frame = QFrame(self, frameShape=QFrame.Box)
-        self.cell_range_frame.setLayout(QFormLayout())
+        self.cell_range_frame.setLayout(QGridLayout())
         # CS label
-        self.CS_label = QLabel(self, text='Select Colony Size column (blank for entire column)')
-        self.cell_range_frame.layout().addRow(self.CS_label, None)
+        self.CS_label = QLabel(self, text='Select Colony Size column (blank for entire column)', wordWrap=True)
+        self.cell_range_frame.layout().addWidget(self.CS_label, 0, 0, 1, 2)
         # CS start
         self.CS_start_label = QLabel(self, text='From:')
+        self.cell_range_frame.layout().addWidget(self.CS_start_label, 1, 0)
         self.CS_start_textbox = QLineEdit(self, placeholderText="A1")
-        self.cell_range_frame.layout().addRow(self.CS_start_label, self.CS_start_textbox)
+        self.cell_range_frame.layout().addWidget(self.CS_start_textbox, 1, 1)
         # CS end
         self.CS_end_label = QLabel(self, text='To:')
+        self.cell_range_frame.layout().addWidget(self.CS_end_label, 2, 0)
         self.CS_end_textbox = QLineEdit(self, placeholderText="None")
-        self.cell_range_frame.layout().addRow(self.CS_end_label, self.CS_end_textbox)
+        self.cell_range_frame.layout().addWidget(self.CS_end_textbox, 2, 1)
         # GR label
-        self.GR_label = QLabel(self, text='Select Growth Rate column (blank for entire column)')
-        self.cell_range_frame.layout().addRow(self.GR_label, None)
+        self.GR_label = QLabel(self, text='Select Growth Rate column (blank for entire column)', wordWrap=True)
+        self.cell_range_frame.layout().addWidget(self.GR_label, 0, 2, 1, 2)
         # GR start
         self.GR_start_label = QLabel(self, text='From:')
+        self.cell_range_frame.layout().addWidget(self.GR_start_label, 1, 2)
         self.GR_start_textbox = QLineEdit(self, placeholderText="A1")
-        self.cell_range_frame.layout().addRow(self.GR_start_label, self.GR_start_textbox)
+        self.cell_range_frame.layout().addWidget(self.GR_start_textbox, 1, 3)
         # GR end
         self.GR_end_label = QLabel(self, text='To:')
+        self.cell_range_frame.layout().addWidget(self.GR_end_label, 2, 2)
         self.GR_end_textbox = QLineEdit(self, placeholderText="None")
-        self.cell_range_frame.layout().addRow(self.GR_end_label, self.GR_end_textbox)
-
+        self.cell_range_frame.layout().addWidget(self.GR_end_textbox, 2, 3)
+        # Add section above to left column
         left_column.addWidget(self.cell_range_frame)
 
-        # Options grid
-        self.options_grid = QFormLayout()
-
+        # --Options frame--
+        # Region where the user selects parameters for the CVP
+        left_column.addWidget(QLabel(self, text='Parameter selection', styleSheet='font-weight: 600'))
+        self.options_frame = QFrame(self, frameShape=QFrame.Box)
+        self.options_frame.setLayout(QFormLayout())
+        # Max colony size parameter
         self.maxbin_colsize_label = QLabel(self, text='Max binned colony size')
         self.maxbin_colsize_num = QSpinBox(self, minimum=0, value=5, maximum=20, singleStep=1)
-        self.options_grid.addRow(self.maxbin_colsize_label, self.maxbin_colsize_num)
-
+        self.options_frame.layout().addRow(self.maxbin_colsize_label, self.maxbin_colsize_num)
+        # Number of bins parameter
         self.nbins_label = QLabel(self, text='Number of bins for remaining population')
         self.nbins_num = QSpinBox(self, minimum=0, value=5, maximum=20, singleStep=1)
-        self.options_grid.addRow(self.nbins_label, self.nbins_num)
-
+        self.options_frame.layout().addRow(self.nbins_label, self.nbins_num)
+        # Number of runs parameter
         self.nruns_label = QLabel(self, text='Number of independent runs to perform')
         self.nruns_num = QSpinBox(self, minimum=0, value=10, maximum=100, singleStep=1)
-        self.options_grid.addRow(self.nruns_label, self.nruns_num)
-
+        self.options_frame.layout().addRow(self.nruns_label, self.nruns_num)
+        # Number of repeats parameter
         self.nrepeats_label = QLabel(self, text='Number of repeated samples for each run/CS')
         self.nrepeats_num = QSpinBox(self, minimum=0, value=10, maximum=100, singleStep=1)
-        self.options_grid.addRow(self.nrepeats_label, self.nrepeats_num)
-
+        self.options_frame.layout().addRow(self.nrepeats_label, self.nrepeats_num)
+        # Sample size parameter
         self.samplesize_label = QLabel(self, text='Sample size')
         self.samplesize_num = QSpinBox(self, minimum=0, value=20, maximum=100, singleStep=1)
-        self.options_grid.addRow(self.samplesize_label, self.samplesize_num)
+        self.options_frame.layout().addRow(self.samplesize_label, self.samplesize_num)
+        # Add section above to left column
+        left_column.addWidget(self.options_frame)
 
-        left_column.addLayout(self.options_grid)
-
+        # --Plot frame--
+        # Region where the button to plot is located, as well as the calculated AAC
+        left_column.addWidget(QLabel(self, text='Plot options', styleSheet='font-weight: 600'))
+        self.plot_frame = QFrame(self, frameShape=QFrame.Box)
+        self.plot_frame.setLayout(QGridLayout())
         # Plot button
-        self.plot_btn = QPushButton(self, text='Generate CVP')
-        self.plot_btn.clicked.connect(self.dynafit_run)
-        left_column.addWidget(self.plot_btn)
+        self.plot_button = QPushButton(self, text='Plot CVP with above parameters')
+        self.plot_button.clicked.connect(self.dynafit_run)
+        self.plot_frame.layout().addWidget(self.plot_button, 0, 0, 1, 2)
+        # AAC label and number
+        self.area_above_curve_label = QLabel(self, text='Measured area above curve:')
+        self.plot_frame.layout().addWidget(self.area_above_curve_label, 1, 0)
+        self.area_above_curve_number = QLabel(self, text='None')
+        self.plot_frame.layout().addWidget(self.area_above_curve_number, 1, 1)
+        left_column.addWidget(self.plot_frame)
 
-        area_above_curve_layout = QHBoxLayout()
-        self.AAC_label = QLabel(self, text='Measured area above curve:')
-        self.AAC_num = QLabel(self, text='None')
-        area_above_curve_layout.addWidget(self.AAC_label)
-        area_above_curve_layout.addWidget(self.AAC_num)
-        left_column.addLayout(area_above_curve_layout)
+        # ### RIGHT COLUMN
 
-
-        # CVP canvas
+        # --Plot canvas--
+        # Region where plots are displayed
         self.fig = Figure(facecolor="white")
-        self.CVP_ax = self.fig.add_axes([0.1, 0.2, 0.85, 0.75])
-        self.hist_ax = self.fig.add_axes([0.05, 0.0, 0.9, 0.1])
-        self.hist_ax.set_axis_off()
         self.canvas = Canvas(self.fig)
         self.canvas.setParent(self)
+        # Axes instance used to plot CVP
+        self.CVP_ax = self.fig.add_axes([0.1, 0.2, 0.85, 0.75])
+        # Axes instance used to plot population histogram
+        self.histogram_ax = self.fig.add_axes([0.05, 0.0, 0.9, 0.1])
+        self.histogram_ax.set_axis_off()
+        # Add canvas above to right column
         right_column.addWidget(self.canvas)
+        # Add a navigation bar to right column
         right_column.addWidget(Navbar(self.canvas, self.frame))
-        columns.setStretch(1, 100)
-        main_layout.setStretch(1, 100)
+
+        # Set column stretch so that only plot gets rescaled with GUI
+        columns.setStretch(1, 10)
 
     def load_data_dialog(self):
         """load"""
@@ -200,27 +215,27 @@ class DynaFitGUI(QMainWindow):
             'sample_size': self.samplesize_num.value(),
             'fig': self.fig,
             'cvp_ax': self.CVP_ax,
-            'hist_ax': self.hist_ax,
+            'hist_ax': self.histogram_ax,
         }
 
     def dynafit_setup(self):
         self.CVP_ax.clear()
-        self.hist_ax.clear()
-        self.plot_btn.setText('Plotting...')
-        self.plot_btn.setEnabled(False)
+        self.histogram_ax.clear()
+        self.plot_button.setText('Plotting...')
+        self.plot_button.setEnabled(False)
 
     def dynafit_raised_exception(self, e):
         self.show_error(e)
         self.CVP_ax.clear()
-        self.hist_ax.clear()
+        self.histogram_ax.clear()
 
     def dynafit_no_exceptions_raised(self):
         self.canvas.draw()
 
     def dynafit_cleanup(self):
-        self.plot_btn.setText('Generate CVP')
-        self.plot_btn.setEnabled(True)
-        self.hist_ax.set_axis_off()
+        self.plot_button.setText('Generate CVP')
+        self.plot_button.setEnabled(True)
+        self.histogram_ax.set_axis_off()
 
     def show_error(self, error):
         text = f'{error.__class__.__name__}:\n{error}'
