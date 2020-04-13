7e6a251cc550f9079cd2157495d91334eb1d4e52
Juliano Faccioni <julianofaccioni@gmail.com>
2020-04-13 15:30:41 -0300

    Added test to utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,9 +8,9 @@ def get_start_end_values(array: np.ndarray) -> Tuple[float, float]:
     return array[0], array[-1]
 
 
-def get_missing_coord(x1: float, y1: float, x2: float, angular_coefficient: float = -1) -> float:
+def get_missing_coord(x1: float, y1: float, x2: float, angular_coefficient: float = -1.0) -> float:
     """Returns the y2 coordinate at the point (x2, y2) of a line which has an angular coefficient of
     "angular_coefficient" and passes through the point (x1, y1)."""
     linear_coefficient = y1 - (angular_coefficient * x1)
-    y2 = linear_coefficient - x2
+    y2 = linear_coefficient + (x2 * angular_coefficient)
     return y2
---
+++ test/test_utils.py
@@ -0,0 +1,48 @@
+import unittest
+from src.utils import get_start_end_values, get_missing_coord
+import numpy as np
+
+
+class TestUtilsModule(unittest.TestCase):
+    """Tests the utils.py module."""
+
+    def test_get_start_end_values_two_or_more_elements_in_array(self) -> None:
+        arr = np.array([1, 2])
+        return_value = get_start_end_values(array=arr)
+        self.assertEqual(len(return_value), 2)
+        self.assertEqual(arr[0], return_value[0])
+        self.assertEqual(arr[-1], return_value[1])
+
+    def test_get_start_end_values_only_one_elements_in_array(self) -> None:
+        arr = np.array([5])
+        return_value = get_start_end_values(array=arr)
+        self.assertEqual(len(return_value), 2)
+        self.assertEqual(arr[0], return_value[0])
+        self.assertEqual(arr[0], return_value[1])
+
+    def test_get_start_end_values_raises_index_error_with_empty_array(self) -> None:
+        arr = np.array([])
+        with self.assertRaises(IndexError):
+            get_start_end_values(array=arr)
+
+    def test_get_missing_coord_y_equals_x_line(self) -> None:
+        # line -> y = x
+        p1 = (0, 0)
+        x1, y1 = p1
+        p2 = (1, 1)
+        x2, expected_y2 = p2
+        actual_y2 = get_missing_coord(x1=x1, y1=y1, x2=x2, angular_coefficient=1.0)
+        self.assertEqual(expected_y2, actual_y2)
+
+    def test_get_missing_coord_y_equals_minus_two_x_plus_three_line(self) -> None:
+        # line -> y = -2x + 3
+        p1 = (0, 3)
+        x1, y1 = p1
+        p2 = (5, -7)
+        x2, expected_y2 = p2
+        actual_y2 = get_missing_coord(x1=x1, y1=y1, x2=x2, angular_coefficient=-2.0)
+        self.assertEqual(expected_y2, actual_y2)
+
+
+if __name__ == '__main__':
+    unittest.main()
