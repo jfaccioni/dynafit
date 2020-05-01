f4740d820abfd0bc9bb169c6eaf6fa1670c15765
Juliano Faccioni <julianofaccioni@gmail.com>
2020-05-01 09:18:41 -0300

    Release 0.3.0 - tested the entire code, made some bug fixes & refactoring related to testing.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme:
 
 setup(
     name="dynafit",
-    version="0.2.0",
+    version="0.3.0",
     author="Juliano Luiz Faccioni",
     author_email="julianofaccioni@gmail.com",
     description="A package to perform DynaFit analysis through a GUI.",
